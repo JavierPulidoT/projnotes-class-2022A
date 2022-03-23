670cb5a6e14915248851102d1983e378da8dc004
JavierPulidoT <javierp.pulido@gmail.com>
2022-03-23 08:53:49 -0600

    Configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+//Import dependencia path
+//dependencia del core de Node
+const path = requiere('path');
+
+mudule.exports = {
+    //1.Especificar el archivo de entrada
+    entry: './client/index.js',
+    //2.Especificar el archivo de salida
+    output: {
+        //2.1 Va a requerir la ruta absoluta de la salida
+        path:path.resolve(__dirname,'public'),
+        //2.2 Nombre del archivo de salida
+        filename:'bundle.js'
+    },
+    //3.configurando el servidor de desarrollo
+    devServer:{
+        //3.1 Folder de archivos estaticos
+        static: path.join(__dirname, 'public'),
+        //3.2 puerto en el servidor de desarrollo 
+        //de WP (Webpack)
+        port: 8080,
+        //3.3 Definiendo host
+        host: 'localhost'
+    }
+}

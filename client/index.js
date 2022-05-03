aaa902b02d1470fa45a2ba410f43c2d6a2db7ba2
JavierPulidoT <javierp.pulido@gmail.com>
2022-05-02 22:58:41 -0500

    S28 ABnB Configurado Ok

--- client/index.js
+++ client/index.js
@@ -1,28 +1,31 @@
+/* eslint-disable */
+/* eslint-disable no-console */
+
 // Incorporando estilos
 // a mi bundle
-import './styles/style.css'
-import './styles/mystyle.css'
+import './styles/style.css';
+import './styles/mystyle.css';
 
-console.log("🎁 Fron-End Working!!!");
+console.log('🎁 Fron-End Working!!!');
 
-//default parameters ES6/2015
-let show = (m="😋") => {  //around funtion 
- alert(m)
-}   
+// default parameters ES6/2015
+const show = (m = '😋') => { // around funtion
+  alert(m);
+};
 show();
-//Asincronia...
-//Promises
-function resolveAfter2Seconds(){  //mostrara un string
-    return new Promise(resolve => {
-        setTimeout(() =>{
-            resolve('funtion resolve')  
-        },2000) //2segundos
-    });
+// Asincronia...
+// Promises
+function resolveAfter2Seconds() { // mostrara un string
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('funtion resolve');
+    }, 2000); // 2segundos
+  });
 }
 
-async function asyncCall(){   //invoca una funcion asincrona, 
-    console.log("Calling async funtion!!!");
-    const result = await resolveAfter2Seconds(); //await invoca promise como si fuera sincrono
-    console.log(result);//Imprime "funtion resolve" en la consola
+async function asyncCall() { // invoca una funcion asincrona,
+  console.log('Calling async funtion!!!');
+  const result = await resolveAfter2Seconds(); // await invoca promise como si fuera sincrono
+  console.log(result);// Imprime "funtion resolve" en la consola
 }
 asyncCall();

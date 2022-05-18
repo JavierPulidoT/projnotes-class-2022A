211efff19256ca5052ee2844bb25d3f4ac82f628
JavierPulidoT <javierp.pulido@gmail.com>
2022-05-18 08:36:11 -0500

    S38 Barra de Navegacion Movil

--- client/index.js
+++ client/index.js
@@ -1,32 +1,14 @@
 /* eslint-disable no-console */
+/* global M */
 
 // Incorporando estilos
 // a mi bundle
 import './styles/mystyle.css';
 
-console.log('🎁 Fron-End Working!!!');
-
-// default parameters ES6/2015
-const show = (m = '😋') => {
-  console.log(m);
-};
-show();
-
-// Asincronia...
-// Promises
-function resolveAfter2Seconds() {
-  // mostrara un string
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('funtion resolve');
-    }, 2000); // 2segundos
-  });
-}
-
-async function asyncCall() {
-  // invoca una funcion asincrona,
-  console.log('Calling async funtion!!!');
-  const result = await resolveAfter2Seconds(); // await invoca promise como si fuera sincrono
-  console.log(result); // Imprime "funtion resolve" en la consola
-}
-asyncCall();
+/* Inicializando elementos de Materializecss */
+document.addEventListener('DOMContentLoaded', () => {
+  // Obteniendo la referencia a la barra de navegacion lateral
+  const sideNavs = document.querySelectorAll('.sidenav');
+  // M...objeto que me da materialize
+  M.Sidenav.init(sideNavs);
+});

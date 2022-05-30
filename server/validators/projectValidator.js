382419d6546e3e272578494f7c7ac83d728fffda
JavierPulidoT <javierp.pulido@gmail.com>
2022-05-30 08:56:28 -0500

    S45 Validacion registrar proyecto Pte 1

---
+++ server/validators/projectValidator.js
@@ -0,0 +1,23 @@
+// 1 Importaremos la biblioteca de validacion
+import * as Yup from 'yup';
+
+// 2 Crear el esquema de validacion
+const projectShema = Yup.object().shape({
+  name: Yup.string().required('Se requiere un nombre para el Proyecto'),
+  description: Yup.string()
+    .max(500, 'La descripcion esta limitada a 500 caracteres')
+    .required('Se requiere una descripcion para el proyecto'),
+});
+
+// 3 Creamos el middleware d evalidacion
+const getProject = (req) => {
+  // Extraemos la info del formulario
+  const { name, description } = req.body;
+  // Armar el Objecto con los datos del proyecto
+  return {
+    name,
+    description,
+  };
+};
+
+export { projectShema, getProject };
